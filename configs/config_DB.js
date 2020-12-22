6be4be60f5394bcd841031cff1afe267cf4210df
khanhhua2405 <71120689+khanhhua2405@users.noreply.github.com>
2020-12-22 17:44:22 +0700

    Add files via upload

---
+++ configs/config_DB.js
@@ -0,0 +1,16 @@
+module.exports = {
+    localdb: {
+        urldb : 'mongodb+srv://thuykhanh:12345@cluster0.isiw4.mongodb.net/shopkhanh1?retryWrites=true&w=majority',
+        dbname : "shopkhanh1",
+        dbusername : "",
+        dbpassword : "",
+    },
+
+    clouddb: {
+        urldb : 'mongodb+srv://thuykhanh:12345@cluster0.isiw4.mongodb.net/shopkhanh1?retryWrites=true&w=majority',
+        dbname : "shopkhanh1",
+        dbusername : "",
+        dbpassword : "",
+    }
+
+};
